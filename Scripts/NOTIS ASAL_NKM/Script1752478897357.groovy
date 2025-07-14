c3cc4228ebe79123dd8eca424f43c7c40192d98b
aaror <aaror@Aarorn_QA>
2025-07-14 16:47:09 +0800

    first code

---
+++ Scripts/NOTIS ASAL_NKM/Script1752478897357.groovy
@@ -0,0 +1,32 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+
+WebUI.openBrowser('http://172.24.65.22:8081')
+WebUI.maximizeWindow()
+
+WebUI.waitForElementVisible(findTestObject('Object Repository/username'), 0)
+WebUI.setText(findTestObject('Object Repository/username'),'user.pk@perkeso.gov.my')
+
+
+WebUI. waitForElementVisible(findTestObject('Object Repository/Password'), 0)
+WebUI.setText(findTestObject('Object Repository/Password'),'skssr1234')
+
+WebUI.click(findTestObject('Object Repository/sign in button'))
+
